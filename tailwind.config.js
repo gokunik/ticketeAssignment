155cbf6136e89fc8c0ed44d91e993c37d1ba8e82
Nitesh Khatri <nitesh@usedatabrain.com>
2024-02-04 16:01:07 +0530

    chore: initial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--primary-font)", "sans-serif"],
+        roboto: ["var(--secondary-font)", "serif"],
+      },
+    },
   },
   plugins: [],
-};
+});
